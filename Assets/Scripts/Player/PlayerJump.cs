b70228b297f147ba3e4d2b0bee0f9d5942f973d1
LlucGuardiola <arceus88205@gmail.com>
2025-04-02 19:52:22 +0200

    padreando a hyux

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -9,14 +9,21 @@ public class PlayerJump : MonoBehaviour
     private bool canJump;
     private Rigidbody2D _rigidbody;
     private CollisionDetection _collisionDetection;
+    int CollisionPos => _collisionDetection.CollisionPos;
 
     private float coyoteTime = 0.2f;
     private float coyoteTimeCounter;
+
+    private bool count;
+    private float counter;
+
+    [HideInInspector] public bool IsWallSliding => _collisionDetection.IsTouchingFront;
 
     [HideInInspector] public bool IsTouchingGround => _collisionDetection.IsGrounded;
 
     void Start()
     {
+        counter = 0;
         _rigidbody = GetComponent<Rigidbody2D>();
         _collisionDetection = GetComponent<CollisionDetection>();
     }
@@ -35,13 +42,30 @@ public class PlayerJump : MonoBehaviour
         {
             canJump = false;
         }
+
+        if (IsWallSliding)
+        {
+            canJump = true;
+        }
+
+        Count();
     }
 
     public void OnJump()
     {
         if (!canJump && coyoteTimeCounter < 0) return;
 
+
         var vel = new Vector2(_rigidbody.linearVelocity.x, JumpStrengh);
+
+        if (IsWallSliding && !IsTouchingGround) 
+        {
+            GetComponent<PlayerMovement>().CanMove = false;
+            vel = new Vector2(-CollisionPos * JumpStrengh / 3, JumpStrengh);
+            count = true;
+            counter = 0;
+        }
+
         _rigidbody.linearVelocity = vel;
         canJump = false;
 
@@ -49,5 +73,18 @@ public class PlayerJump : MonoBehaviour
         //{
         //    audioSource.PlayOneShot(JumpSound);
         //}
+    }
+
+    private void Count()
+    {
+        if (!count) return;
+
+        counter += Time.deltaTime;
+
+        if (counter >= 0.4f)
+        {
+            GetComponent<PlayerMovement>().CanMove = true;
+            count = false;
+        }
     }
 }

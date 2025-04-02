d4cf761294cf5bd83d5ed9b87719a339275565c1
ErikSalguero <erodriguezsal@edu.tecnocampus.cat>
2025-04-02 21:18:28 +0200

    Creation of bullet and Distance Attack scripts

---
+++ Assets/Scripts/Enemies/DistanceAttack.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DistanceAttack : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    [SerializeField] private float speed;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Shoot()
+    {
+     
+    }
+
+}

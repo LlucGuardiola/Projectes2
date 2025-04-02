fb4d51d07a1e062e1a73961b72868a7692836be8
ErikSalguero <erodriguezsal@edu.tecnocampus.cat>
2025-04-03 00:17:08 +0200

    Bullet script logic

--- Assets/Scripts/Enemies/Bullet.cs
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float damage = 1.0f;
+   // private Rigidbody2D _rigidbody;
+    //private GameObject player;
+    private Vector2 direction;
+    
+
     void Start()
     {
-        
+   //     _rigidbody = GetComponent<Rigidbody2D>();
+      //  _rigidbody.velocity = direction * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+    }
+
+    public void SetDirection(Vector2 targetPosition)
+    {
+        direction = (targetPosition - targetPosition).normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Health health = collision.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
     }
 }

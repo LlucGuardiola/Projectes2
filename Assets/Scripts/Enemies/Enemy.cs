e67a7b91b031ce4101b590f9090ac44aa8218ef6
LlucGuardiola <arceus88205@gmail.com>
2025-05-12 17:47:43 +0200

    communist (nunca supe como se escribia)

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -13,14 +13,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] public float PatrolSpeed;
     [SerializeField] public float ChaseSpeed;
 
-    /*[HideInInspector]*/ public bool HasToChase;
-    /*[HideInInspector]*/ public bool IsChasing;
-    /*[HideInInspector]*/ public bool IsPatrolling;
-    /*[HideInInspector]*/ public bool LookingForward;
+    [HideInInspector] public bool HasToChase;
+    [HideInInspector] public bool IsChasing;
+    [HideInInspector] public bool IsPatrolling;
+    [HideInInspector] public bool LookingForward;
     public bool InRange;
     public bool DistanceAttack;
     public bool MeleeAttack;
-    /*[HideInInspector]*/ public bool IsAttacking;
+    [HideInInspector] public bool IsAttacking;
     public bool PatrollingDisabled;
     private Animator animator;
 

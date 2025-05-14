c510611ab5d9b58c411156586ad7ee41e1677fd5
LlucGuardiola <arceus88205@gmail.com>
2025-05-14 16:54:00 +0200

    gambaclat

---
+++ Assets/Scripts/Enviroment/LockedDoors.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LockedDoors : MonoBehaviour
+{
+    [SerializeField] private int[] cards;
+
+    private void Update()
+    {
+        
+    }
+    private void CheckCards()
+    {
+        foreach (var card in PlayerInventory.PlayerCards)
+        {
+            
+        }
+    }
+}

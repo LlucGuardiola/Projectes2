24af400be109827d9436d56693cc1b71d5312a3e
LlucGuardiola <arceus88205@gmail.com>
2025-05-16 13:06:01 +0200

    doors almost done

--- Assets/Scripts/Enviroment/LockedDoors.cs
+++ Assets/Scripts/Enviroment/LockedDoors.cs
@@ -3,16 +3,47 @@ using UnityEngine;
 public class LockedDoors : MonoBehaviour
 {
     [SerializeField] private int[] cards;
+    private bool[] values;
 
     private void Update()
     {
+        values = new bool[cards.Length];
         
+        for (int i = 0; i < values.Length; i++)
+        {
+            values[i] = false;
+        }
     }
-    private void CheckCards()
+    private bool CheckCards()
     {
         foreach (var card in PlayerInventory.PlayerCards)
         {
-            
+            for (int i = 0; i < cards.Length; i++)
+            {
+                if (!values[i])
+                {
+                    if (card == cards[i])
+                    {
+                        values[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        foreach (var value in values)
+        {
+            if (!value) return false;
+        }
+
+        return true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CheckCards())
+        {
+            GetComponent<Collider2D>().enabled = false;
         }
     }
 }

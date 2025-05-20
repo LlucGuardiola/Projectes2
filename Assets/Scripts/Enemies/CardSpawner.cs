c85abfc9f6df5d18bc4d51533722f003c1110d49
LlucGuardiola <arceus88205@gmail.com>
2025-05-20 22:00:34 +0200

    cumame cumeameeeeeeeeeeeeeee sisisi ohhh siiii cum cum yesssss la polischiiiiia

--- Assets/Scripts/Enemies/CardSpawner.cs
+++ Assets/Scripts/Enemies/CardSpawner.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class CardSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject cardPrefab; 
+    [SerializeField] private Sprite sprite;
     [SerializeField] private Color color;
     [SerializeField] private int index;
 
     public void InstantiateCard()
     {
         GameObject card = Instantiate(cardPrefab, transform.position, Quaternion.identity);
-        card.GetComponent<SpriteRenderer>().color = color;
+        card.GetComponent<SpriteRenderer>().color = color;        
+        card.GetComponent<SpriteRenderer>().sprite = sprite;
         card.GetComponent<Card>().Index = index;
     }
 }

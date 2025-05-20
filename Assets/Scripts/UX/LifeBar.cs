1f27a569b853b2687f8a55322e75b476bd1f2139
Jan <jrevillam@edu.tecnocampus.cat>
2025-05-21 00:36:48 +0200

    barra de vida
    barça

---
+++ Assets/Scripts/UX/LifeBar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+    public Health playerHealth;
+    public SpriteRenderer lifeBarRenderer;
+    public Sprite[] lifeSprites; 
+
+    private float maxLife;
+
+    void Start()
+    {
+        maxLife = playerHealth.Life;
+        UpdateLifeDisplay();
+    }
+
+    void Update()
+    {
+        if (lifeBarRenderer.sprite != GetCurrentLifeSprite())
+        {
+            UpdateLifeDisplay();
+        }
+    }
+
+    void UpdateLifeDisplay()
+    {
+        lifeBarRenderer.sprite = GetCurrentLifeSprite();
+    }
+
+    Sprite GetCurrentLifeSprite()
+    {
+        float lifePercentage = playerHealth.Life / maxLife;
+        int spriteIndex = Mathf.FloorToInt(lifePercentage * (lifeSprites.Length - 1));
+        return lifeSprites[Mathf.Clamp(spriteIndex, 0, lifeSprites.Length - 1)];
+    }
+}

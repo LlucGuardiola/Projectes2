4eb400173317ccde6eed32e2fd109e66a4d19d45
LlucGuardiola <arceus88205@gmail.com>
2025-05-21 12:21:12 +0200

    objects deactivate on trigger enter (final)

--- Assets/Scripts/Enviroment/EnableWorldObject.cs
+++ Assets/Scripts/Enviroment/EnableWorldObject.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public class EnableWorldObject : MonoBehaviour
 {
-    public GameObject[] gameObjects;
-    public GameObject[] triggers;
+    public GameObject[] triggerObjects;  
+    public GameObject[] objectsToActivate; 
+    private GameObject player;
 
+    private void Start()
+    {
+        foreach (GameObject obj in objectsToActivate)
+        {
+            obj.SetActive(false);
+        }
+    }
 
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < triggerObjects.Length; i++)
+        {
+            if (collision.gameObject == triggerObjects[i])
+            {
+                objectsToActivate[i].SetActive(true);
+            }
+        }
+    }
 }

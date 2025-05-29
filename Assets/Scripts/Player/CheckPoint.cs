f1e00c5a2e8ff557d2d6afbd8d97d31e6f5da5a7
Jan R <jrevillam@edu.tecnocampus.cat>
2025-05-29 16:47:20 +0200

    checkpoint torch

--- Assets/Scripts/Player/CheckPoint.cs
+++ Assets/Scripts/Player/CheckPoint.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     private GameObject player;
+    [SerializeField] private GameObject torch;
 
     private void Start()
     {
         player = GameObject.Find("Player");
+
+        if (torch != null)
+        {
+            torch.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == player) CheckpointManager.SetCheckpoint(transform.position);
+
+        if (torch != null)
+        {
+            torch.SetActive(true);
+        }
     }
 }

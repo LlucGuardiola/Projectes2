a96cd8ba0ca52ba2e7c68857e4ffd6ddad2fab7d
LlucGuardiola <arceus88205@gmail.com>
2025-06-04 18:55:21 +0200

    changes

--- Assets/Scripts/Player/CheckpointManager.cs
+++ Assets/Scripts/Player/CheckpointManager.cs
@@ -7,11 +7,13 @@ public class CheckpointManager : MonoBehaviour
 
     private static Vector3 savedPosition;
     private GameObject player;
+    public static bool Respawning;
 
     void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+        Respawning = false;
     }
 
     private void Start()
@@ -26,13 +28,17 @@ public class CheckpointManager : MonoBehaviour
 
     public void StartRespawn()
     {
+        if (Respawning) return;
         CameraFade.StartFade(true, 1f);
         Invoke("Respawn", 2f / CameraFade.SpeedScale / 2f);
+        Respawning = true; 
+        
     }
 
     private void Respawn()
     {
         player.transform.position = savedPosition;
         player.GetComponent<Health>().RestartLife();
+        Respawning = false;
     }
 }
